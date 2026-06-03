6e76bea7fbae39f65e97221f1ae8b13c1c618158
Yi Wang <s2533013@kpuenta.ca>
2026-06-03 11:45:28 -0700

    grid drawn

--- 1333_Template/Assets/1333_RTS/StudentWork/Scripts/GameManager.cs
+++ 1333_Template/Assets/1333_RTS/StudentWork/Scripts/GameManager.cs
@@ -4,4 +4,9 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private UnitManager _unitManager;
     [SerializeField] private GridManager _gridManager;
+
+    private void Awake()
+    {
+        _gridManager.InitializeGrid();
+    }
 }
--- 1333_Template/Assets/1333_RTS/StudentWork/Scripts/GridManager.cs
+++ 1333_Template/Assets/1333_RTS/StudentWork/Scripts/GridManager.cs
@@ -1,12 +1,13 @@
 using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEditor;
 
 public class GridManager : MonoBehaviour
 {
     //var to allow us to plug in our GridSettings scriptableOBJ
     [SerializeField] private GridSettings _gridSettings;
-    public GridSettings gridSettings => _gridSettings;
+    public GridSettings GridSettings => _gridSettings;
 
     //2D array of GridNoide structs that represents our grid.
     private GridNode[,] _gridNodes;
@@ -24,11 +25,117 @@ public class GridManager : MonoBehaviour
         //initializing our array of GridNode structs using the dimensions from the Scriptable Objects
         _gridNodes = new GridNode[_gridSettings.GridSizeX, _gridSettings.GridSizeY];
 
+        //Nest for Loop to iterate over all _gridNodes
+        //at each grid position, instantiate a new GridNode struct, give it some default values and add it
+        //to our gridNode 2D array
         for (int x = 0; x < _gridSettings.GridSizeX; x++) 
         { 
             for (int y = 0; y < _gridSettings.GridSizeY; y++)
             {
+                /*
+                 * if(_gridSettings.UseXZPlane)
+                 *      worldPos = new Vector3(x, 0, y) * _gridSettings.NodeSize;
+                 * else
+                 *      worldPos = new Vector3(x, y, 0) * _gridSettings.NodeSize;
+                 *      
+                 * [(condition) ? result if true : result if false]
+                 */
+                Vector3 worldPos = _gridSettings.UseXZPlane
+                    ? new Vector3(x, 0, y) * _gridSettings.NodeSize
+                    : new Vector3(x, y, 0) * _gridSettings.NodeSize;
 
+                GridNode node = new GridNode
+                {
+                    Name = $"Cell_{x + _gridSettings.GridSizeX * x + y}",
+                    WorldPos = worldPos,
+                    Walkable = true, //Default all nodes to be walkable, modify later
+                    Weight = 1 //Default weight, useful for varied terrain costs
+                };
+
+                _gridNodes[x, y] = node;
+            }
+        }
+        IsInitialized = true;
+    }
+
+#if UNITY_EDITOR
+    private void PopulateDebugList()
+    {
+        //clear our debug list of _gridNodes
+        //Then for each already existing GridNode, create a new GridNode, grab its info, set it to the newly created GridNode
+        //for debug purposes
+        AllNodes.Clear();
+
+        for(int x = 0; x < _gridSettings.GridSizeX; x++)
+        {
+            for(int y = 0; y < _gridSettings.GridSizeY; y++)
+            {
+                GridNode node = _gridNodes[x, y];
+                AllNodes.Add(new GridNode
+                {
+                    Name = $"Cell_{x}+{y}",
+                    WorldPos = node.WorldPos,
+                    Walkable = node.Walkable,
+                    Weight = node.Weight
+                });
+            }
+        }
+    }
+#endif
+
+    //Function to retrieve GridNode data efficiently
+    public GridNode GetNode(int x, int y)
+    {
+        //first check if function arguments are out of bounds of the grid
+        //otherwise return the proper GridNode
+        if (x < 0 || x >= _gridSettings.GridSizeX || y < 0 || y >= _gridSettings.GridSizeY)
+            throw new System.IndexOutOfRangeException("Grid node indices out of range");
+
+        return _gridNodes[x, y];
+    }
+
+    //Example setter for walkability, expanded in future logic
+    //public void SetWalkable(int x, int y, bool walkable)
+    //{
+    //    _gridNodes[x, y]
+    //}
+
+    //Efficient visualization using Gizmoes, toggleable through Unity Editor
+    private void OnDrawGizmos()
+    {
+        if (_gridNodes == null || GridSettings == null) return;
+
+        Gizmos.color = Color.green;
+
+        //Draw the gridnode gizmos, size is 90% of GridNode Size for visual clarity
+        for(int x = 0; x < _gridSettings.GridSizeX; x++)
+        {
+            for(int y = 0; y < _gridSettings.GridSizeY; y++)
+            {
+                GridNode node = _gridNodes[x, y];
+                Gizmos.color = node.Walkable ? Color.green : Color.red;
+                Gizmos.DrawWireCube(node.WorldPos, Vector3.one * GridSettings.NodeSize * 0.9f);
+            }
+        }
+    }
+
+    //Create a custom editor button that, when pressed, calls PopulateDebugList and refreshes the Editor GUI
+    [CustomEditor(typeof(GridManager))]
+    public class GridManagerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            //first draw the normal inspector GUI
+            DrawDefaultInspector();
+
+            //then look at the GridManager class this is attached to and call the PopulateDebugList function
+            GridManager grid = (GridManager)target;
+            if (grid.IsInitialized)
+            {
+                if(GUILayout.Button("Refresh Grid Debug View"))
+                {
+                    grid.PopulateDebugList();
+                }
             }
         }
     }
